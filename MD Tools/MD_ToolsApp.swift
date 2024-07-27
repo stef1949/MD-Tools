ee9bac200fb21012ac943aed8b59e4b6525b5e77
Stef1949 <stef1949.sr@gmail.com>
2024-07-27 18:07:18 +0100

    Initial Commit

---
+++ MD Tools/MD_ToolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MD_ToolsApp.swift
+//  MD Tools
+//
+//  Created by Stephan Ritchie on 27/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct MD_ToolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
